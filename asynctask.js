f5b8e861e41f5d0c87ace2f576c37a40f76d471d
sakshirao8577 <raosakshi8577@gmail.com>
2026-03-16 10:23:36 +0530

    Add files via upload

---
+++ asynctask.js
@@ -0,0 +1,16 @@
+var fs=require("fs");
+fs.writeFile("help.txt","You are creating a file",(e)=>{
+    if(e) throw e;
+    else{
+       fs.appendFile("help.txt","You are appending the data",(e)=>{
+           if(e) throw e;
+           else{
+            fs.readFile("help.txt","utf-8",(e,data)=>{
+                if(e) throw e;
+                console.log("Thank you for using my program");
+            }
+            )
+           };
+    })
+    };
+});
